5bb272f6af8f290ff8aab80890d27836c43ffc9c
niranth <niranthcr@gmail.com>
2019-07-18 22:58:07 +0530

    initial commit

---
+++ deck.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"strings"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValue := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValue {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error  {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main()  {
+	cards := newDeck()
+
+	cards.saveToFile("my_file")
+}
